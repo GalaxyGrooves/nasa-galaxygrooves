3f72283cd199f952168a5e0b968d0d16488507f2
drkchn <57195399+drkchn@users.noreply.github.com>
2023-10-08 03:55:51 -0700

    Location coordinates

---
+++ location_coordinates.py
@@ -0,0 +1,18 @@
+#####
+##### This code snippet contains the hard coded values for the space coordinates
+#####
+
+# dictionary to set the values based on our 4 celestial objects
+# key = predetermined space object as an index value
+# value = 2-tuple space coordinate in the format (right ascension, declination)
+
+# 0 = NGC 1365
+# 1 = NCG 1097
+# 2 = M45
+# 3 = M33
+space_coordinates = {
+    "0": (53.40155, -36.1404),
+    "1": (41.57937, -30.27489),
+    "2": (56.86909, 24.10531),
+    "3": (23.46204, 30.66022)
+}
